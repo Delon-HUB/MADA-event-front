cec46cc8e1b76d15669007d43d079b87b50ee393
Delon <nicolas123delon@gmail.com>
2025-10-20 10:50:01 +0300

    chore: configure to use quasar

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import 'quasar/dist/quasar.css'
+import '@quasar/extras/material-icons/material-icons.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import { Notify, Quasar } from 'quasar'
 
 const app = createApp(App)
+
 app.use(createPinia())
+app.use(Quasar, { plugins: { Notify } })
 
 app.mount('#app')
