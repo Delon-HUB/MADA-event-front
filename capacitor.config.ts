9d1b840382251eda4f99bf66fc2a262238729fb8
Delon <nicolas123delon@gmail.com>
2025-10-20 10:53:29 +0300

    chore: add capacitor android to build app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.madaEvent.app',
+  appName: 'mada-event-front',
+  webDir: 'dist'
+};
+
+export default config;
