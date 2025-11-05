daa18395d4ac6b10aa106dfec36b3e352d1d9f0d
Delon <nicolas123delon@gmail.com>
2025-11-05 22:58:27 +0300

    feat: dashboard counter event

--- src/pages/organizer/Dashboard.vue
+++ src/pages/organizer/Dashboard.vue
@@ -1,3 +1,88 @@
-<template>ORGANIZER DASHBOARD</template>
+<template>
+  <q-page>
+    <q-header bordered class="page">
+      <q-toolbar>
+        <q-toolbar-title class="text-black text-bold">
+          <q-icon name="bar_chart_4_bars" />Statistiques</q-toolbar-title
+        >
+      </q-toolbar>
+    </q-header>
+    <div class="row text-center">
+      <div class="col">
+        <q-card flat bordered class="q-ma-xs">
+          <q-knob
+            :max="total"
+            :min="0"
+            show-value
+            class="text-white q-ma-md"
+            v-model="$eventStore.coming.length"
+            size="64px"
+            :thickness="0.2"
+            color="light-blue"
+            center-color="light"
+            track-color="grey-3"
+            ><span class="text-bold text-light-blue">{{ $eventStore.coming.length }}</span></q-knob
+          >
+          <p class="text-bold">À venir</p>
+        </q-card>
+      </div>
+      <div class="col">
+        <q-card flat bordered class="q-ma-xs">
+          <q-knob
+            :max="total"
+            :min="0"
+            show-value
+            class="text-white q-ma-md"
+            v-model="$eventStore.inProgress.length"
+            size="64px"
+            :thickness="0.2"
+            color="green"
+            center-color="light"
+            track-color="grey-3"
+            ><span class="text-bold text-green">{{ $eventStore.inProgress.length }}</span></q-knob
+          >
+          <p class="text-bold">Encours</p>
+        </q-card>
+      </div>
+      <div class="col">
+        <q-card flat bordered class="q-ma-xs">
+          <q-knob
+            :max="total"
+            :min="0"
+            show-value
+            class="text-white q-ma-md"
+            v-model="$eventStore.terminated.length"
+            size="64px"
+            :thickness="0.2"
+            color="red"
+            center-color="light"
+            track-color="grey-3"
+            ><span class="text-bold text-red">{{ $eventStore.terminated.length }}</span></q-knob
+          >
+          <p class="text-bold">Terminé</p>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useEventStore } from '@/stores/Event.store'
+import { watch, ref } from 'vue'
+
+const $eventStore = useEventStore()
+const total = ref<number>($eventStore.all.length)
+
+watch(
+  () => $eventStore.all,
+  () => {
+    total.value = $eventStore.all.length
+  },
+)
+</script>
+
+<style scoped>
+.page {
+  background-color: #f1efe3;
+}
+</style>
