31ad278bc4f0ae08662a09e8531b08cbac972d1a
Delon <nicolas123delon@gmail.com>
2025-11-05 11:46:30 +0300

    feat: install dayjs for relative time (eg: 18hours ago)

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -9,21 +9,21 @@
 
       <q-item-section>
         <q-item-label>Nicolas Delon</q-item-label>
-        <q-item-label caption> il y a 12 minutes </q-item-label>
+        <q-item-label caption> {{ createdAt }} </q-item-label>
       </q-item-section>
       <q-space />
       <q-item-section class="text-right text-bold">
-        <q-item-label> <q-icon color="green-8" name="payments" /> 10.000ar</q-item-label>
+        <q-item-label>
+          <q-icon color="green-8" name="payments" />
+          {{ props.event.price <= 0 ? 'gratuit' : props.event.price + 'ar' }}</q-item-label
+        >
       </q-item-section>
     </q-item>
-    <q-img
-      src="https://i.pinimg.com/1200x/06/30/e6/0630e63a37d7ef817f4705d1627bd89d.jpg"
-      :ratio="16 / 9"
-    />
+    <q-img v-if="props.event.photo" :src="photo" :ratio="16 / 9" />
     <div class="row items-center q-mx-xs">
-      <p class="text-h6 text-bold"><q-icon name="event" />Mahaleo</p>
+      <p class="text-h6 text-bold"><q-icon name="event" />{{ event.title }}</p>
       <q-space />
-      <p><q-icon name="place" color="red" />Fianarantsoa</p>
+      <p><q-icon name="place" color="red" />{{ province }}</p>
     </div>
     <q-card-actions>
       <q-btn no-caps flat round color="positive" icon="receipt" label="Reserver" />
@@ -44,16 +44,28 @@
       <div v-show="expanded">
         <q-separator />
         <q-card-section class="text-subtitle2">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt blanditiis sit
-          voluptates dolores nihil? Minima sapiente, aperiam laboriosam facilis corporis libero
-          officiis in eveniet, modi illum vitae voluptas vel amet!
+          {{ props.event.description }}
         </q-card-section>
       </div>
     </q-slide-transition>
   </q-card>
 </template>
 <script setup lang="ts">
+import type { IEvent } from '@/interfaces/IEvent'
 import { ref } from 'vue'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/fr'
+
+dayjs.extend(relativeTime)
+dayjs.locale('fr')
 
 const expanded = ref(false)
+const props = defineProps<{ event: IEvent }>()
+
+const photo = ref<string>(`${import.meta.env.VITE_API_URL}/${props.event.photo}`)
+const location = props.event.location.split(',')
+const province = ref<string>(location[0]!)
+
+const createdAt = dayjs(props.event.createdAt).fromNow()
 </script>
