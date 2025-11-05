7f205ad1a5d66666c65bddc1f09a29f3d6263c61
Delon <nicolas123delon@gmail.com>
2025-11-05 23:51:31 +0300

    feat: fetch only in progress and coming events

--- src/pages/client/Home.vue
+++ src/pages/client/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <q-list>
-    <Event :event="event" v-for="event in $eventStore.all" class="q-ma-xs" />
+    <Event :event="event" v-for="event in events" class="q-ma-xs" />
   </q-list>
 </template>
 <script setup lang="ts">
 import Event from '@/components/Event.vue'
 import { useEventStore } from '@/stores/Event.store'
+import { computed } from 'vue'
 
 const $eventStore = useEventStore()
+const events = computed(() => $eventStore.inProgress.concat($eventStore.coming))
 </script>
