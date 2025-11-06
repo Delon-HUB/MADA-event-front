3ee0ff70cb6171df0f450348f59e5bb25f0c1cfd
Delon <nicolas123delon@gmail.com>
2025-11-06 21:27:09 +0300

    feat: ticket store

---
+++ src/stores/Ticket.store.ts
@@ -0,0 +1,31 @@
+import { secureAPI } from '@/instances/axios'
+import { defineStore } from 'pinia'
+import type { ICreatePaymentDto as IPayment } from '@/interfaces/IPayment'
+import type { ITicket } from '@/interfaces/ITicket'
+import { ref } from 'vue'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import isBetween from 'dayjs/plugin/isBetween'
+
+dayjs.extend(relativeTime)
+dayjs.extend(isBetween)
+
+export const userTicketStore = defineStore('ticket', () => {
+  let tickets = ref<ITicket[]>([])
+
+  const init = async () => {
+    await getMyTickets()
+  }
+
+  const getMyTickets = async () => {
+    const response = await secureAPI.post('/ticket/mine')
+    tickets.value = response.data as ITicket[]
+  }
+
+  const buy = async (payment: Partial<IPayment>) => {
+    const response = await secureAPI.post('/payment', payment)
+    return response
+  }
+
+  return { init, getMyTickets, buy, tickets }
+})
