9080da46b660943eeb9be8e9913f8231c6bd434f
Delon <nicolas123delon@gmail.com>
2025-11-07 17:08:28 +0300

    feat: design dashboard

--- src/pages/organizer/Dashboard.vue
+++ src/pages/organizer/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="page">
+  <q-page class="page q-mx-sm">
     <q-header bordered class="page header">
       <q-toolbar>
         <q-toolbar-title class="text-black text-bold">
@@ -7,62 +7,89 @@
         >
       </q-toolbar>
     </q-header>
-    <div class="row text-center">
-      <div class="col">
-        <q-card flat bordered class="q-ma-xs">
-          <q-knob
-            :max="total"
-            :min="0"
-            show-value
-            class="text-white q-ma-md"
-            v-model="$eventStore.coming.length"
-            size="64px"
-            :thickness="0.2"
-            color="black"
-            center-color="light"
-            track-color="grey-3"
-            ><span class="text-bold text-black">{{ $eventStore.coming.length }}</span></q-knob
-          >
-          <p class="text-bold">À venir</p>
-        </q-card>
+    <q-card flat>
+      <div class="row text-center">
+        <p class="text-overline full-width">17 événements en total</p>
       </div>
-      <div class="col">
-        <q-card flat bordered class="q-ma-xs">
-          <q-knob
-            :max="total"
-            :min="0"
-            show-value
-            class="text-white q-ma-md"
-            v-model="$eventStore.inProgress.length"
-            size="64px"
-            :thickness="0.2"
-            color="green"
-            center-color="light"
-            track-color="grey-3"
-            ><span class="text-bold text-green">{{ $eventStore.inProgress.length }}</span></q-knob
-          >
-          <p class="text-bold">Encours</p>
-        </q-card>
+      <div class="row text-center">
+        <div class="col">
+          <q-card flat bordered class="q-ma-xs">
+            <q-knob
+              :max="total"
+              :min="0"
+              show-value
+              class="text-white q-ma-md"
+              v-model="$eventStore.coming.length"
+              size="64px"
+              :thickness="0.2"
+              color="black"
+              center-color="light"
+              track-color="grey-3"
+              ><span class="text-bold text-black">{{ $eventStore.coming.length }}</span></q-knob
+            >
+            <p class="text-bold">À venir</p>
+          </q-card>
+        </div>
+        <div class="col">
+          <q-card flat bordered class="q-ma-xs">
+            <q-knob
+              :max="total"
+              :min="0"
+              show-value
+              class="text-white q-ma-md"
+              v-model="$eventStore.inProgress.length"
+              size="64px"
+              :thickness="0.2"
+              color="green"
+              center-color="light"
+              track-color="grey-3"
+              ><span class="text-bold text-green">{{ $eventStore.inProgress.length }}</span></q-knob
+            >
+            <p class="text-bold">Encours</p>
+          </q-card>
+        </div>
+        <div class="col">
+          <q-card flat bordered class="q-ma-xs">
+            <q-knob
+              :max="total"
+              :min="0"
+              show-value
+              class="text-white q-ma-md"
+              v-model="$eventStore.terminated.length"
+              size="64px"
+              :thickness="0.2"
+              color="red"
+              center-color="light"
+              track-color="grey-3"
+              ><span class="text-bold text-red">{{ $eventStore.terminated.length }}</span></q-knob
+            >
+            <p class="text-bold">Terminé</p>
+          </q-card>
+        </div>
       </div>
-      <div class="col">
-        <q-card flat bordered class="q-ma-xs">
-          <q-knob
-            :max="total"
-            :min="0"
-            show-value
-            class="text-white q-ma-md"
-            v-model="$eventStore.terminated.length"
-            size="64px"
-            :thickness="0.2"
-            color="red"
-            center-color="light"
-            track-color="grey-3"
-            ><span class="text-bold text-red">{{ $eventStore.terminated.length }}</span></q-knob
-          >
-          <p class="text-bold">Terminé</p>
-        </q-card>
-      </div>
-    </div>
+    </q-card>
+    <q-card flat class="q-mt-md">
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon color="red" name="receipt" />
+        </q-item-section>
+
+        <q-item-section>Billet vendu</q-item-section>
+        <q-item-section side>
+          <q-item-label>350</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon color="light-green" name="payments" />
+        </q-item-section>
+
+        <q-item-section>Gains total</q-item-section>
+        <q-item-section side>
+          <q-item-label>150000ar</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-card>
   </q-page>
 </template>
 
@@ -89,5 +116,10 @@ watch(
 
 .page {
   background-color: #f1efe3;
+}
+
+.my-card {
+  width: 20%;
+  height: 100px;
 }
 </style>
