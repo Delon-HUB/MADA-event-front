60ada30c34a18942ebed70bb757629c8a21e67ee
Delon <nicolas123delon@gmail.com>
2025-11-14 13:22:06 +0300

    feat: design event component

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,6 +1,6 @@
 <template>
   <q-card flat bordered>
-    <q-item>
+    <q-item class="q-pa-none">
       <q-item-section class="text-bold">
         <q-item-label>
           <p class="text-bold">
@@ -33,7 +33,7 @@
                 dense
                 color="red"
                 text-color="white"
-                icon="event"
+                icon="receipt"
                 >{{
                   props.event.ticketAvailable! > 0
                     ? `${props.event.ticketAvailable} billets restant `
@@ -45,16 +45,11 @@
         </q-item-label>
       </q-item-section>
       <q-space />
-      <q-item-section class="text-right text-bold">
-        <q-item-label>
-          <q-icon name="payments" color="green" />{{
-            props.event.price <= 0
-              ? 'gratuit'
-              : addSeparatorNumber(props.event.price, 3, ' ') + ' Ar'
-          }}
-        </q-item-label>
-        <q-item-label caption>
-          {{ event.category }}
+      <q-item-section class="text-right col-4 text-bold" style="justify-content: start">
+        <q-item-label> <q-icon name="place" color="red" />{{ province }} </q-item-label>
+        <q-item-label overline class="text-bold">
+          <q-icon name="calendar_month" />
+          {{ new Date(event.startDate).toLocaleDateString() }}
         </q-item-label>
       </q-item-section>
     </q-item>
@@ -73,11 +68,17 @@
         <q-item-label caption> {{ createdAt }} </q-item-label>
       </q-item-section>
       <q-space />
-      <q-item-section class="text-right">
-        <q-item-label> <q-icon name="place" color="red" />{{ province }} </q-item-label>
-        <q-item-label overline class="text-bold">
-          <q-icon name="calendar_month" />
-          {{ new Date(event.startDate).toLocaleDateString() }}
+
+      <q-item-section class="text-right text-bold">
+        <q-item-label>
+          <q-icon name="payments" color="green" />{{
+            props.event.price <= 0
+              ? 'gratuit'
+              : addSeparatorNumber(props.event.price, 3, ' ') + ' Ar'
+          }}
+        </q-item-label>
+        <q-item-label caption>
+          {{ event.category }}
         </q-item-label>
       </q-item-section>
     </q-item>
